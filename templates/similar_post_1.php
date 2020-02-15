d75a09aaabb093eb65fd3b0a266bcae146f78b90
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2020-02-15 20:00:27 +0100

    Similar Post added

---
+++ templates/similar_post_1.php
@@ -0,0 +1,15 @@
+<!--<div class="container">
+							<div class="row d-flex flex-row align-items-end">
+
+								
+								<div class="col-lg-3 col-md-6 similar_post_col">
+									<div class="similar_post trans_200">
+										<a href="post.php">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
+									</div>
+								</div>
+
+								
+								
+							</div>
+						</div>
+                        -->
---
+++ templates/similar_post_2.php
@@ -0,0 +1,13 @@
+<div class="home_slider_next_container">
+							<div class="home_slider_next" style="background-image:url(images/home_slider_next.jpg)">
+								<div class="home_slider_next_background trans_400"></div>
+								<div class="home_slider_next_content trans_400">
+									<div class="home_slider_next_title">next</div>
+									
+									<div class="home_slider_next_link"><?php echo "Still working on this";?></div>
+									
+								</div>
+							</div>
+						</div>
+
+					</div> 
